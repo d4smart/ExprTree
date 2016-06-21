d00fc840fef7b38e34ebdcee885719d53948c959
d4smart <d4smart@foxmail.com>
2016-06-21 09:32:19 +0800

    Add notes

--- ExprTree.c
+++ ExprTree.c
@@ -11,6 +11,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+//二叉树节点结构体
 typedef struct _node
 {
     char ele;
@@ -18,31 +19,30 @@ typedef struct _node
     struct _node *right;
 } Node;
 
-Node *root;             //表达式树根节点
-int base;               //用于对优先级进行浮动变换，每进入一层括号加一
+Node *root;                         //表达式树根节点
+int base;                           //用于对优先级进行浮动变换，每进入一层括号加一
 
 int IsOperator(char c);             //判断字符是否是运算符
 char *MoveToEnd(char *str);         //将文件指针移动到字符串末尾
-int GetPriority(char c, int flag);  //获取运算符的优先级
+int GetPriority(char c);            //获取运算符的优先级
 char *FriLstOpr(char *str);         //从尾部开始向前找到第一个优先级最低的运算符
 int Check(char *expr);              //检查输入的表达式是否合法
 Node *MakeTree(Node *node, char *str);  //根据表达式生成表达式树（二叉树）
-double CalExpr(Node *node);            //根据表达式树计算表达式的值
+double CalExpr(Node *node);         //根据表达式树计算表达式的值
 int IsNumber(char *str);            //判断字符串是否为纯数字
 void RemoveBrackets(char *str);     //去除表达式中多余的括号（包含整个式子的）
 
 int main()
 {
-    char expr[200];
-    //Node *head;
-    //char *cur, *opr;
+    char expr[200]; //用于存储算数表达式
 
     printf("Enter the expression to caculate, press ctrl+c to quit: ");
 
-    while(scanf("%s", expr) != EOF)
+    while(scanf("%s", expr) != EOF) //处理主循环
     {
         base = 0;
 
+        //表达式检查没有通过
         if(!Check(expr))
         {
             printf("Expression invalid!\n");
@@ -50,19 +50,24 @@ int main()
             continue;
         }
 
-        root = (Node *)malloc(sizeof(Node));
-        RemoveBrackets(expr);
-        root = MakeTree(root, expr);
+        root = (Node *)malloc(sizeof(Node));    //为根节点分配内存
+        RemoveBrackets(expr);           //去除表达式中多余的括号
+        root = MakeTree(root, expr);    //根据表达式生成表达式树
 
-        double result = CalExpr(root);
-        printf("Result is %f.\n", result);
+        double result = CalExpr(root);  //计算结果
+        printf("Result is %f.\n\n", result);
         
         printf("Enter the expression to caculate, press ctrl+c to quit: ");
     }
 
-    printf("\nQuiting... Bye!\n");
+    printf("\n\nQuiting... Bye!\n\n");
 }
 
+//检查输入的表达式是否合法
+//输入：表达式字符串首地址
+//输出：判断值，1是0否
+//原理：表达式中左右括号必须对应，且顺序遍历过程中左括号个数一定>=右括号个数
+//      最后左括号个数等于右括号个数
 int Check(char *expr)
 {
     int pos = 0;
@@ -71,45 +76,61 @@ int Check(char *expr)
 
     while(*c)
     {
+        //判断括号并记数
         if (*c == '(')
             pos++;
         if (*c == ')')
             pos--;
+
+        //小于0则右括号个数大于左括号，表达式不合法
         if (pos < 0)
             return 0;
-        c++;
+
+        c++;    //游标自增
     }
 
+    //最后左括号个数等于右括号个数则验证通过
     if (pos != 0)
         return 0;
     else 
         return 1;
 }
 
+//去除表达式中多余的括号（对应范围为整个式子）
+//输入：带多余括号的表达式
+//输出：去除表达式中多余的括号
+//例如：（（1+2）*3）->（1+2）*3， （1+2）*（3+4）->（1+2）*（3+4）
+//原理：触底指括号个数对应相等（抵消），带多余括号的表达式
+//      在顺序遍历时一定会触底且只触底一次
 void RemoveBrackets(char *str)
 {
     char *pos;
-    pos = str;
+    pos = str;  //声明游标并将游标移到表达式开头
 
+    //带多余括号的表达式第一个字符一定是"("，据此判断
     if (*pos != '(')
         return;
 
-    int inner = 0, BottomTime = 0;
+    int inner = 0, BottomTime = 0;  //抵消计数，触底计数
     while(*pos)
         {
+            //对括号计数
             if (*pos == '(')
                 inner++;
             if (*pos == ')')
                 inner--;
 
+            //若触底则触底计数加1
             if (inner == 0)
                 BottomTime++;
-            pos++;
+
+            pos++;  //游标自增
         }
 
     if (BottomTime != 1)
         return;
 
+    //去除括号的处理
     pos = str;
     while(*(pos+2))
     {
@@ -120,10 +141,14 @@ void RemoveBrackets(char *str)
     *(pos+1) = '\0';
 }
 
+//根据表达式生成表达式树
+//输入：表达式字符串
+//输出：构造出的表达式树根节点
 Node *MakeTree(Node *node, char *str)
 {
-    char lstr[100], rstr[100];
-    //node = (Node *)malloc(sizeof(Node));
+    char lstr[100], rstr[100];  //运算符左右的字符串
+
+    //若表达式为数字，则创建节点存放数字并返回节点
     if(IsNumber(str))
     {
         int num = atoi(str);
@@ -133,41 +158,44 @@ Node *MakeTree(Node *node, char *str)
         return node;
     }
 
-    char *cur = MoveToEnd(str);
-    char *opr = FriLstOpr(cur);
-    node->ele = *opr;
+    char *cur = MoveToEnd(str); //移动到字符串末尾
+    char *opr = FriLstOpr(cur); //找出优先级最低的运算字符
+    node->ele = *opr;           //存储运算符
 
-    //CutExpr(lstr, str, opr);
-    strncpy(lstr, str, opr-str);
-    RemoveBrackets(lstr);
+    strncpy(lstr, str, opr-str);    //切割左边的字符串
+    RemoveBrackets(lstr);           //去除多余括号
     node->left = (Node *)malloc(sizeof(Node));
-    MakeTree(node->left, lstr);
+    MakeTree(node->left, lstr);     //构造左子树
 
-    //CutExpr(rstr, opr+1, cur+1);
-    strncpy(rstr, opr+1, cur-opr);
-    RemoveBrackets(rstr);
+    strncpy(rstr, opr+1, cur-opr);  //切割右边的字符串
+    RemoveBrackets(rstr);           //去除多余括号
     node->right = (Node *)malloc(sizeof(Node));
-    MakeTree(node->right, rstr);
+    MakeTree(node->right, rstr);    //构造右子树
 
     //node = ptr;
-    return node;
+    return node;    //返回树的根节点
 }
 
+//判断字符串是否为纯数字
+//输入：待判断字符串
+//输出：判断结果
 int IsNumber(char *str)
 {
     while(*str)
     {
+        //出现运算符则不是纯数字
         if(IsOperator(*str))
             return 0;
-        str++;
+
+        str++;  //游标自增
     }
 
-    return 1;
+    return 1;   //是纯数字
 }
 
 //判断字符是否是运算符
 //输入：运算字符
-//输出：Bool值，1是0非
+//输出：判断结果
 int IsOperator(char c)
 {
         if (c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')')
@@ -196,19 +224,21 @@ char *MoveToEnd(char *str)
 //输出：运算符地址
 char *FriLstOpr(char *str)
 {
-    char *opr;
-    int Pri = 65536, tPri;
+    char *opr;              //运算符
+    int Pri = 65536, tPri;  //最低优先级，当前优先级
 
     while(*str)
     {
+        //出现括号改变base值
         if (*str == ')')
             base++;
         if (*str == '(')
             base--;
 
+        //出现运算符则与当前优先级最低运算符比较
         if (IsOperator(*str))
         {
-            tPri = GetPriority(*str, 1);
+            tPri = GetPriority(*str);
 
             if(tPri < Pri)
             {
@@ -220,21 +250,28 @@ char *FriLstOpr(char *str)
         str--;
     }
 
-    return opr;
+    return opr; //返回运算符地址
 }
 
+//根据表达式树计算表达式的值
+//输入：表达式树根节点
+//输出：运算结果
 double CalExpr(Node *node)
 {
     double result;
 
+    //节点为数字直接返回数字
     if (!IsOperator(node->ele))
         return node->ele;
 
-    double LeftResult = CalExpr(node->left);
-    double RightResult = CalExpr(node->right);
+    double LeftResult = CalExpr(node->left);    //计算左子树结果
+    double RightResult = CalExpr(node->right);  //计算右子树结果
+
+    //释放树节点内存
     free(node->left);
     free(node->right);
 
+    //运算
     if (node->ele == '+')
     {
         result = LeftResult + RightResult;
@@ -252,14 +289,15 @@ double CalExpr(Node *node)
         result = LeftResult / RightResult;
     }
 
-    return result;
+    return result;  //返回运算结果
 }
 
 //获取运算符的优先级
 //输入：运算符，flag标记
 //输出：优先级，数值越大优先级越高
-int GetPriority(char c, int flag)
+int GetPriority(char c)
 {
+    //运算符合法，则返回浮动优先级
     if (c == '+' || c == '-')
         return base * 10 + 1;
     else if (c == '*' || c == '/')
